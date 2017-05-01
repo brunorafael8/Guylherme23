5ec5342dd12703ed2cef727ebd8a2bce39c18927
Bruno Rafael <bruninho_lito@hotmail.com>
2017-05-01 19:53:26 -0300

    add gulp

---
+++ gulpfile.js
@@ -0,0 +1,56 @@
+var gulp = require('gulp')
+  ,imagemin = require('gulp-imagemin')
+  ,clean = require('gulp-clean')
+  ,concat = require('gulp-concat')
+  ,uglify = require('gulp-uglify')
+  ,browserSync = require('browser-sync')
+  ,csslint = require('gulp-csslint')
+  ,autoprefixer = require('gulp-autoprefixer')
+
+gulp.task('default', ['copy'], function() {
+    gulp.start('build-img');
+});
+
+gulp.task('copy', ['clean'], function() {
+    return gulp.src('**/*')
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('clean', function() {
+    return gulp.src('dist')
+        .pipe(clean());
+});
+
+gulp.task('build-img', function() {
+
+  return gulp.src('dist/img/**/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('dist/img'));
+})
+
+gulp.task('serve', ['sass'], function() {
+
+    browserSync.init({
+        server: "./app"
+    });
+
+    gulp.watch("app/scss/*.scss", ['sass']);
+    gulp.watch("app/*.html").on('change', browserSync.reload);
+});
+    gulp.watch('**/*').on('change', browserSync.reload);
+
+    gulp.watch('js/**/*.js').on('change', function(event) {
+        console.log("Linting " + event.path);
+        gulp.src(event.path)
+            .pipe(jshint())
+            .pipe(jshint.reporter(jshintStylish));
+    });
+
+    gulp.watch('css/**/*.css').on('change', function(event) {
+        console.log("Linting " + event.path);
+        gulp.src(event.path)
+            .pipe(csslint())
+            .pipe(csslint.reporter());
+    });    
+
+});
