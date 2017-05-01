d657ef93deea16d043248cb041c7a2b6c053f4d5
Bruno Rafael <bruninho_lito@hotmail.com>
2017-05-01 20:36:26 -0300

    gulp add

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,13 @@
+'use strict';
+
 var gulp = require('gulp')
   ,imagemin = require('gulp-imagemin')
   ,clean = require('gulp-clean')
   ,concat = require('gulp-concat')
   ,uglify = require('gulp-uglify')
   ,browserSync = require('browser-sync')
-  ,csslint = require('gulp-csslint')
   ,autoprefixer = require('gulp-autoprefixer')
+  ,sass = require('gulp-sass');
 
 gulp.task('default', ['copy'], function() {
     gulp.start('build-img');
@@ -27,30 +29,21 @@ gulp.task('build-img', function() {
     .pipe(imagemin())
     .pipe(gulp.dest('dist/img'));
 })
-
 gulp.task('serve', ['sass'], function() {
 
     browserSync.init({
-        server: "./app"
+        server: "./"
     });
 
-    gulp.watch("app/scss/*.scss", ['sass']);
-    gulp.watch("app/*.html").on('change', browserSync.reload);
+    gulp.watch("scss/*.scss", ['sass']);
+    gulp.watch("*.html").on('change', browserSync.reload);
 });
-    gulp.watch('**/*').on('change', browserSync.reload);
+gulp.task('sass', function () {
+    gulp.src('./sass/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./css'));
+});
 
-    gulp.watch('js/**/*.js').on('change', function(event) {
-        console.log("Linting " + event.path);
-        gulp.src(event.path)
-            .pipe(jshint())
-            .pipe(jshint.reporter(jshintStylish));
-    });
-
-    gulp.watch('css/**/*.css').on('change', function(event) {
-        console.log("Linting " + event.path);
-        gulp.src(event.path)
-            .pipe(csslint())
-            .pipe(csslint.reporter());
-    });    
-
+gulp.task('sass:watch', function () {
+    gulp.watch('./sass/**/*.scss', ['sass']);
 });
