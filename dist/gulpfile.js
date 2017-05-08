d1c0e144c6407a3c90c35051b5aea5bc3e2a4cbe
Bruno Rafael <bruninho_lito@hotmail.com>
2017-05-08 11:55:25 -0300

    firebase deploy

---
+++ dist/gulpfile.js
@@ -0,0 +1,49 @@
+'use strict';
+
+var gulp = require('gulp')
+  ,imagemin = require('gulp-imagemin')
+  ,clean = require('gulp-clean')
+  ,concat = require('gulp-concat')
+  ,uglify = require('gulp-uglify')
+  ,browserSync = require('browser-sync')
+  ,autoprefixer = require('gulp-autoprefixer')
+  ,sass = require('gulp-sass');
+
+gulp.task('default', ['copy'], function() {
+    gulp.start('build-img');
+});
+
+gulp.task('copy', ['clean'], function() {
+    return gulp.src('**/*')
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('clean', function() {
+    return gulp.src('dist')
+        .pipe(clean());
+});
+
+gulp.task('build-img', function() {
+
+  return gulp.src('dist/img/**/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('dist/img'));
+})
+gulp.task('serve', ['sass'], function() {
+
+    browserSync.init({
+        server: "./"
+    });
+
+    gulp.watch("./sass/*.scss", ['sass']).on('change', browserSync.reload);
+    gulp.watch("*.html").on('change', browserSync.reload);
+});
+gulp.task('sass', function () {
+    gulp.src('./sass/style.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./css'));
+});
+
+gulp.task('sass:watch', function () {
+    gulp.watch('./sass/style.scss', ['sass']);
+});
